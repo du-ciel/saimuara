6ee3a4d871bc38d754f9ec7103c64bb617569347
du-ciel <bountislazy@gmail.com>
2026-09-15 23:44:49 +0700

    fix: Vercel 500 error and update composer lock

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0777, true);
+}
+
 // Mengarahkan semua request Vercel (dari folder api) ke file utama Laravel (di folder public)
 require __DIR__ . '/../public/index.php';
